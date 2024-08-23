c8b36caa3aaa59e20899b750661f360931abd756
Rina Ebeid <rina.ebeid@shipwire.com>
2024-08-22 18:30:42 -0700

    added env-specific manifests

--- app/app.py
+++ app/app.py
@@ -4,6 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def get_release_version() -> str:
+    version = os.environ.get('RELEASE', 'undefined')
+    return f"Accessing the {version} of the app\n"
+
+
 @app.route('/say/<something>')
 def say_something(something: str) -> str:
     return f"<h1>\\(^o^)/ {something}!</h1>"
